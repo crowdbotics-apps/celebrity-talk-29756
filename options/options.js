b68decdaab9aac4547cb4c08fb13e6071ed68966
Crowdbotics <team@crowdbotics.com>
2021-08-12 10:50:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "celebrity_talk_29756Identifier",
+  url: "https://celebrity_talk_29756Identifier.botics.co",
+  api: "https://celebrity_talk_29756Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
